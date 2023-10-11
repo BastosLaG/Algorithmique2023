d94c637bce2171c44dbf4e624bc622d3da906211
bastien capiaux <bastien.capiaux@gmail.com>
2023-10-12 00:06:56 +0200

    lol le tp3

--- tp/tp2/TP2.c
+++ tp/tp2/TP2.c
@@ -10,9 +10,29 @@ struct vecfloat {
 typedef struct vecfloat vec_t;
 
 vec_t creeretremplir (int nb);
-vec_t inserev (vec_t lr, vec_t aplacer);
-vec_t triiv (vec_t l);
-vec_t bullv (vec_t l);
+vec_t inserev (vec_t lr);
+vec_t bullev (vec_t lr);
+void quickSort(float arr[], int low, int high);
+int partition(float arr[], int low, int high);
+vec_t qsvec(vec_t lr);
+vec_t selectionSortv(vec_t lr) {
+    int i, j, min_idx, n;
+    n = lr.nbele;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (lr.v[j] < lr.v[min_idx]) {
+                min_idx = j;
+            }
+        }
+        // Échanger l'élément minimum trouvé avec l'élément actuel de l'itération
+        float temp = lr.v[min_idx];
+        lr.v[min_idx] = lr.v[i];
+        lr.v[i] = temp;
+    }
+    return lr;
+}
+
 
 
 struct cellfloat {
@@ -27,6 +47,9 @@ void affliste (liste l);
 liste insere (liste lr, liste aplacer);
 liste trii (liste l);
 liste bull (liste l);
+liste partitionL(liste l, liste* gauche, liste* droite);
+liste qs(liste l);
+liste selectionSort(liste l);
 
 int main (int argc, char ** argv) {
 	int n, i;
@@ -34,7 +57,7 @@ int main (int argc, char ** argv) {
 	vec_t vec, vect;
 	clock_t td, ta;
 	n = 100;
-	for (i = 0; i < 4-1; i++)
+	for (i = 0; i < 4; i++)
 	{
 		ll = creer(n);
 		lt = NULL;
@@ -46,20 +69,39 @@ int main (int argc, char ** argv) {
 		td = clock();
 		lt = bull (ll);
 		ta = clock();
-		printf("Tri bulles avec liste	\t taille %d\t temps %d\n\n",n,(int) (ta - td));
+		printf("Tri bulles avec liste	\t taille %d\t temps %d\n",n,(int) (ta - td));
+    ll = creer(n);
+		td = clock();
+		lt = selectionSort(ll);
+		ta = clock();
+		printf("Tri selection avec liste\t taille %d\t temps %d\n",n,(int) (ta - td));
+    ll = creer(n);
+		td = clock();
+		lt = qs(ll);
+		ta = clock();
+		printf("Quicksort avec liste	\t taille %d\t temps %d\n\n",n,(int) (ta - td));
 
 		vec = creeretremplir(n);
 		lt = NULL;
 		td = clock();
-		vect = triiv (vec);
+		vect = inserev(vec);
 		ta = clock();
 		printf("Tri insertion avec vec	\t taille %d\t temps %d\n",n,(int) (ta - td));
 		vec = creeretremplir(n);
 		td = clock();
-		vect = bullv (vec);
+		vect = bullev (vec);
 		ta = clock();
-		printf("Tri bulles avec vec	\t taille %d\t temps %d\n\n",n,(int) (ta - td));
-
+		printf("Tri bulles avec vec	\t taille %d\t temps %d\n",n,(int) (ta - td));
+    vec = creeretremplir(n);
+		td = clock();
+    vect = selectionSortv(vec);
+		ta = clock();
+		printf("Tri selection avec vec	\t taille %d\t temps %d\n",n,(int) (ta - td));
+    vec = creeretremplir(n);
+		td = clock();
+    vect = qsvec(vec);
+		ta = clock();
+		printf("Quicksort avec vec	\t taille %d\t temps %d\n\n",n,(int) (ta - td));
 		n *= 10;
 	}
 	
@@ -135,10 +177,10 @@ liste creer (int nb) {
 
 void affliste (liste l)  {
   while (l) {
-    printf("%f \t", l->v);
+    printf("%f, ", l->v);
     l = l->nxt;
   }
-  printf("\n");
+  printf("\n\n");
 }
 
 liste insere (liste lr, liste aplacer) {
@@ -214,13 +256,176 @@ liste bull (liste l) {
   }
   return l;
 }
-
-vec_t insere (vec_t lr, vec_t aplacer){
-
+liste partitionL(liste l, liste* gauche, liste* droite) {
+	if (!l || !l->nxt) {
+		*gauche = l;
+		*droite = NULL;
+		return l;
+	}
+	liste pivot = l;
+	l = l->nxt;
+	pivot->nxt = NULL;
+	liste gauche_head = NULL, gauche_tail = NULL;
+	liste droite_head = NULL, droite_tail = NULL;
+	while (l) {
+		liste suivant = l->nxt;
+		if (l->v <= pivot->v) {
+			if (!gauche_head) {
+				gauche_head = l;
+				gauche_tail = l;
+			} else {
+				gauche_tail->nxt = l;
+				gauche_tail = l;
+			}
+		} else {
+			if (!droite_head) {
+				droite_head = l;
+				droite_tail = l;
+			} else {
+				droite_tail->nxt = l;
+				droite_tail = l;
+			}
+		}
+		l = suivant;
+	}
+	if (gauche_tail) {
+		gauche_tail->nxt = NULL;
+	}
+	if (droite_tail) {
+		droite_tail->nxt = NULL;
+	}
+	*gauche = gauche_head;
+	*droite = droite_head;
+	return pivot;
 }
-vec_t trii (vec_t l){
 
-}
-vec_t bull (vec_t l){
+liste qs(liste l) {
+    if (!l || !l->nxt) {
+        return l;
+    }
 
+    liste gauche = NULL, droite = NULL;
+    liste pivot = partitionL(l, &gauche, &droite);
+
+    gauche = qs(gauche);
+    droite = qs(droite);
+
+    if (gauche) {
+        liste crt = gauche;
+        while (crt->nxt) {
+            crt = crt->nxt;
+        }
+        crt->nxt = pivot;
+    } else {
+        gauche = pivot;
+    }
+
+    pivot->nxt = droite;
+
+    return gauche;
+}   
+
+liste selectionSort(liste l) {
+  if (!l || !l->nxt) {
+    return l;  // Already sorted or empty list
+  }
+
+  liste current = l;
+  liste sorted = NULL;
+
+  while (current) {
+    liste minNode = current;
+    liste minPrev = NULL;
+
+    // Find the minimum node in the remaining unsorted part of the list
+    liste temp = current;
+    while (temp->nxt) {
+      if (temp->nxt->v < minNode->v) {
+        minNode = temp->nxt;
+        minPrev = temp;
+      }
+      temp = temp->nxt;
+    }
+
+    // Remove the minimum node from the unsorted list
+    if (minPrev) {
+      minPrev->nxt = minNode->nxt;
+    } else {
+      current = current->nxt;
+    }
+
+    // Add the minimum node to the sorted list
+    minNode->nxt = sorted;
+    sorted = minNode;
+  }
+
+  return sorted;
 }
+
+vec_t inserev(vec_t w) {
+    int i, j;
+    float temp;
+    for (i = 1; i < w.nbele; i++) {
+        temp = w.v[i];
+        j = i - 1;
+        while (j >= 0 && w.v[j] > temp) {
+            w.v[j + 1] = w.v[j];
+            j--;
+        }
+        w.v[j + 1] = temp;
+    }
+    return w;
+}
+
+vec_t bullev(vec_t w) {
+    int n = w.nbele;
+    int i, j;
+    float temp;
+    for (i = 0; i < n - 1; i++) {
+        for (j = 0; j < n - i - 1; j++) {
+            if (w.v[j] > w.v[j + 1]) {
+                temp = w.v[j];
+                w.v[j] = w.v[j + 1];
+                w.v[j + 1] = temp;
+            }
+        }
+    }
+    return w;
+}
+
+
+
+int partition(float arr[], int low, int high) {
+    float pivot = arr[high];
+    int i = (low - 1);
+
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            float temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    float temp = arr[i + 1];
+    arr[i + 1] = arr[high];
+    arr[high] = temp;
+
+    return (i + 1);
+}
+
+void quickSort(float arr[], int low, int high) {
+    if (low < high) {
+        int pi = partition(arr, low, high);
+
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+vec_t qsvec(vec_t lr) {
+    quickSort(lr.v, 0, lr.nbele - 1);
+    return lr;
+}
+
--- tp/tp3/TP3.c
+++ tp/tp3/TP3.c
@@ -16,8 +16,13 @@ void droite_triviale_verif (int u, int v, monplan pl);
 void droite_br (int u, int v, monplan pl);
 void droite_br_verif (int u, int v, monplan pl);
 
-void droite_rvw (int u, int v, monplan pl);
-void droite_rvw_verif (int u, int v, monplan pl);
+int PGCD(int a, int b);
+void droite_pgcd(int u, int v, monplan pl);
+void droite_pgcd_verif(int u, int v, monplan pl);
+
+char* droite_Green_and_Pittway(int dx, int dy);
+void droite_Green_and_Pittway_verif(int dx, int dy, monplan pl);
+
 
 int main (int argc, char ** argv) {
   int sizex, sizey, dx, dy, i, j;
@@ -68,13 +73,24 @@ int main (int argc, char ** argv) {
   
   memset (plan, ' ', MAX*MAX);
 
-  droite_rvw_verif (11, 3, plan);
-  printf("Rokne\n");
+  droite_pgcd_verif (11, 3, plan);
+  printf("PGCD\n");
   affiche (11, 3, plan);
   
   memset (plan, ' ', MAX*MAX);
 
-  droite_rvw_verif (24, 10, plan);
+  droite_pgcd_verif (24, 10, plan);
+  affiche (24, 10, plan);
+  
+  memset (plan, ' ', MAX*MAX);
+
+  droite_Green_and_Pittway_verif(11, 3, plan);
+  printf("Trace\n");
+  affiche (11, 3, plan);
+  
+  memset (plan, ' ', MAX*MAX);
+
+  droite_Green_and_Pittway_verif (24, 10, plan);
   affiche (24, 10, plan);
 
   t0 = clock();
@@ -98,16 +114,20 @@ int main (int argc, char ** argv) {
   t0 = clock();
   for (i = 0; i < dx; i++) {
     for (j = 0; j < i; j++) {
-      droite_rvw (i, j, plan);
-    }
-    for (j = 0; j < i; j++) {
-      droite_rvw (i, j, plan);
+      	droite_pgcd (i, j, plan);
     }
   }
   t1 = clock();
-  printf("Rokne %d\n", (int) (t1 - t0));
-  
+  printf("Pgcd %d\n", (int) (t1 - t0));
 
+  t0 = clock();
+  for (i = 1; i < dx; i++) {
+    for (j = 1; j < i; j++) {
+      	droite_Green_and_Pittway(i, j);
+    }
+  }
+  t1 = clock();
+  printf("Trace %d\n", (int) (t1 - t0));
 }
 
 void droite_triviale (int u, int v, monplan pl) {
@@ -161,6 +181,7 @@ void droite_br_verif (int u, int v, monplan pl) {
     delta  = incH - u;
     incD   = delta - u;
     for (x = 0, y = 0; x <= u; x++) {
+        pl [x][(int) (((float) x * v / u) + 0.5)] = '.';
         if (delta > 0) {
             y++;
             delta += incD;
@@ -168,17 +189,49 @@ void droite_br_verif (int u, int v, monplan pl) {
         else {
             delta += incH;
         }
-        pl [x][y] = '.';
     }
 }
 
-void droite_rvw (int u, int v, monplan pl) {
-    int x, y, delta, incD, incH;
+int PGCD(int a, int b) {
+	if (a <= 0 || b <= 0) return 0;
+	if(a==b) return a; 
+	else {
+		if(a>b) return PGCD(a-b, b);
+		else return PGCD(a, b-a);
+	}
+}
+void droite_pgcd(int u, int v, monplan pl){
+	int g, ug, vg, x, y, incH, delta, incD;
+	g = PGCD(u,v);
+	if (g == 0) {}
+	else{
+		ug = (u/g);
+		vg = (v/g);
+		incH   = vg << 1;
+		delta  = incH - ug;
+		incD   = delta - ug;
+		for (x = 0, y = 0; x <= u; x++) {
+			if (delta > 0) {
+				y++;
+				delta += incD;
+			}
+			else {
+				delta += incH;
+			}
+		}
+	}
+}
 
-    incH   = v << 1;
-    delta  = incH - u;
-    incD   = delta - u;
+void droite_pgcd_verif(int u, int v, monplan pl){
+	int g, ug, vg, x, y, incH, delta, incD;
+	g = PGCD(u,v);
+	ug = (u/g);
+	vg = (v/g);
+	incH   = vg << 1;
+    delta  = incH - ug;
+    incD   = delta - ug;
     for (x = 0, y = 0; x <= u; x++) {
+        pl [x][(int) (((float) x * v / u) + 0.5)] = '.';
         if (delta > 0) {
             y++;
             delta += incD;
@@ -186,25 +239,72 @@ void droite_rvw (int u, int v, monplan pl) {
         else {
             delta += incH;
         }
-        delta = 2 * u * y + u - 2 * y * x;
     }
 }
 
-void droite_rvw_verif (int u, int v, monplan pl) {
-    int x, y, delta, incD, incH;
+// char* droitebest (dx, dy) {
+// 	dx -= dy;
+// 	s = "0";
+// 	t = "1";
+// 	while (dx != dy) {
+// 		if (dx > dy) {
+// 		dx -= dy;
+// 		t = s . t ^ (-1);
+// 		}
+// 		else {
+// 		dy -= dx;
+// 		s = t . s ^(-1);
+// 		}
+// 	}
+// 	return ( s . t ^ (-1) ) ^dx;
+// }
 
-    incH   = v << 1;
-    delta  = incH - u;
-    incD   = delta - u;
-    for (x = 0, y = 0; x <= u; x++) {
-        if (delta > 0) {
-            y++;
-            delta += incD;
-        }
-        else {
-            delta += incH;
-        }
-        delta = 2 * u * y + u - 2 * y * x;
-        pl [x][y] = '.';
-    }
+char* droite_Green_and_Pittway(int dx, int dy) {
+	int i;
+	dx -= dy;
+	char* s = "0";
+    char* t = "1";
+	char* temp = malloc(sizeof(char) * (strlen(s) + strlen(t) + 1));
+	char* result = malloc(sizeof(char) * (strlen(s) + strlen(t) + 1));
+	while (dx != dy) {
+		if (dx > dy) {
+			dx -= dy;
+			if (sizeof(char) * (strlen(s) + strlen(t) + 1) >= sizeof(temp)) {
+    			temp = (char*)realloc(temp, sizeof(char) * (strlen(s) + strlen(t) + 1));
+			}
+			strcpy(temp, s);
+			strcat(temp, t);
+			t = temp;
+		} else {
+			dy -= dx;
+			if (sizeof(char) * (strlen(s) + strlen(t) + 1) >= sizeof(temp)) {
+    			temp = (char*)realloc(temp, sizeof(char) * (strlen(s) + strlen(t) + 1));
+			}
+			strcpy(temp, s);
+			strcat(temp, t);
+			s = temp;
+		}
+	}
+	result = (char*)realloc(result, sizeof(char) * (strlen(s) + strlen(t) + 1));
+	strcpy(result, s);
+	strcat(result, t);
+    return result;
+}
+
+void droite_Green_and_Pittway_verif(int dx, int dy, monplan pl){
+	char* result = droite_Green_and_Pittway(dx, dy);
+	int i, dxi;
+	int x = 0, y = 0;
+	for (dxi = 0; dxi < dx; dxi++)
+	{
+		for ( i = 0; result[i] != '\0'; i++) {
+			pl[x][(int) (((float) x * dy / dx) + 0.5)] = '.';
+			if (result[i] == '1') {
+				y++;
+			}
+			x++;
+		}
+		pl[x][(int) (((float) x * dy / dx) + 0.5)] = '.';    			
+	}
+	free(result);
 }
